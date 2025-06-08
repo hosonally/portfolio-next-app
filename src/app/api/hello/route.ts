e52a483b74436316965301b469fe434b1437b8a8
Toshi H <60739572+hosonally@users.noreply.github.com>
2025-06-08 21:56:30 +0900

    add text box and api param (#2)

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,6 +1,13 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  return NextResponse.json({ message: 'I am the world you always say hello to!' });
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const person = searchParams.get("person");
+  if (!person) {
+    return NextResponse.json({
+      message: "I am the world you always say hello to!",
+    });
+  } else {
+    return NextResponse.json({ message: `Hi, I am ${person}` });
+  }
 }
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-"use client"
+"use client";
 import { useState } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 export default function Home() {
   const [message, setMessage] = useState("");
+  const [person, setPerson] = useState("");
+
+  const handleOnChangePerson = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPerson(event.target.value);
+  };
 
   const handleClick = async () => {
     try {
-      const res = await fetch("/api/hello");
+      const res = await fetch(
+        `/api/hello?person=${encodeURIComponent(person)}`
+      );
       const data = await res.json();
       setMessage(data.message);
     } catch (error) {
@@ -16,14 +25,29 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-4">
-      <button
-        onClick={handleClick}
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+    <>
+      <Box
+        component="form"
+        sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+        noValidate
+        autoComplete="off"
       >
-        Fetch Message
-      </button>
-      <div className="mt-4 text-lg">{message}</div>
-    </main>
+        <TextField
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          onChange={handleOnChangePerson}
+        />
+      </Box>
+      <main className="flex flex-col items-center justify-center min-h-screen p-4">
+        <button
+          onClick={handleClick}
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          Fetch Message
+        </button>
+        <div className="mt-4 text-lg">{message}</div>
+      </main>
+    </>
   );
 }
