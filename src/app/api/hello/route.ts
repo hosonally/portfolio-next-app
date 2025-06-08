4f6405e43b33b49e21194c00924e2b13861cb959
Toshi H <60739572+hosonally@users.noreply.github.com>
2025-06-08 23:19:01 +0900

    format all with new prettier config (#4)

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,13 +1,13 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
-  const person = searchParams.get("person");
+  const { searchParams } = new URL(request.url)
+  const person = searchParams.get('person')
   if (!person) {
     return NextResponse.json({
-      message: "I am the world you always say hello to!",
-    });
+      message: 'I am the world you always say hello to!',
+    })
   } else {
-    return NextResponse.json({ message: `Hi, I am ${person}` });
+    return NextResponse.json({ message: `Hi, I am ${person}` })
   }
 }
